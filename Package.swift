f0edee12da4dcacca8f0a8346851df056e80dd79
Bradley David Bergeron <brad@bradbergeron.com>
2023-09-17 23:01:24 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     .package(url: "https://github.com/nalexn/ViewInspector", .upToNextMajor(from: "0.9.7")),
-    .package(name: "Stubby", path: "../Stubby"),
+    .package(url: "git@github.com:bdbergeron/Stubby.git", branch: "main"),
   ],
   targets: [
     .target(
