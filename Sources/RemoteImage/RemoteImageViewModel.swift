b738d19b59ab118fb593d8c752845d64afa7df44
Bradley David Bergeron <brad@bradbergeron.com>
2023-09-23 15:57:20 -0400

    Move extensions and helpers to own files

--- Sources/RemoteImage/RemoteImageViewModel.swift
+++ Sources/RemoteImage/RemoteImageViewModel.swift
@@ -169,30 +169,3 @@ final class RemoteImageViewModel: ObservableObject {
   }
 
 }
-
-// MARK: initWithCache
-
-extension RemoteImageViewModel {
-  /// Create a new ``RemoteImageViewModel`` instance.
-  ///
-  /// A ``URLSession`` will be constructed using the ``URLSessionConfiguration.default`` configuration and the specified ``cache``.
-  /// - Parameters:
-  ///   - url: Image URL.
-  ///   - cache: Cache instance to use.
-  ///   - configuration: A ``RemoteImageConfiguration`` object to use for configuring this model.
-  convenience init(
-    url: URL?,
-    cache: URLCache,
-    configuration: RemoteImageConfiguration)
-  {
-    self.init(
-      url: url,
-      urlSession: URLSession(
-        configuration: {
-          let configuration = URLSessionConfiguration.default
-          configuration.urlCache = cache
-          return configuration
-        }()),
-      configuration: configuration)
-  }
-}
