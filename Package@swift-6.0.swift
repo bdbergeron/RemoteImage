05bbd817e0d5e1eda77789213abce032226d59e9
Bradley David Bergeron <brad@bradbergeron.com>
2025-10-25 14:50:44 -0700

    Add Swift 6 package manifest

---
+++ Package@swift-6.0.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+  name: "RemoteImage",
+  platforms: [
+    .macOS(.v13),
+    .iOS(.v16),
+  ],
+  products: [
+    .library(name: "RemoteImage", targets: ["RemoteImage"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/bdbergeron/Stubby", .upToNextMajor(from: "1.0.0")),
+    .package(url: "https://github.com/nalexn/ViewInspector", .upToNextMajor(from: "0.10.0")),
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.0"),
+  ],
+  targets: [
+    .target(
+      name: "RemoteImage",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "RemoteImageTests",
+      dependencies: [
+        .target(name: "RemoteImage"),
+        .product(name: "ViewInspector", package: "ViewInspector"),
+        .product(name: "Stubby", package: "Stubby"),
+      ],
+      resources: [
+        .process("Resources")
+      ]
+    ),
+  ],
+  swiftLanguageModes: [.v6]
+)
